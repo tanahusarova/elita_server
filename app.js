64febf2fa841792ecfb985451c34fea01710c6e4
Tatiana Husárová <tatianahusarova@MacBook-Air-uzivatela-Tatiana-2.local>
2023-04-29 20:47:20 +0200

    Commit for hosting

--- app.js
+++ app.js
@@ -42,8 +42,12 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
+
 // error handler
 app.use(function(err, req, res, next) {
+
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -51,6 +55,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+  
 });
 
 module.exports = app;
--- model/user.js
+++ model/user.js
@@ -1,19 +1,19 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-
+/*
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV == "dev" ? false : {
     rejectUnauthorized: false
   }
+  */
   
-  /*
   user: 'postgres',
   host: 'localhost',
   database: 'elitadb',
   password: 'heslo1234',
   port: 5432,
-  */
+
 
 });
 const bcrypt = require('bcrypt');
